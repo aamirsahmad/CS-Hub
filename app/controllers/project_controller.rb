919de21e3ee550133169a1652940a903807f56e3
Philip Alekseev <phil.alekseev@gmail.com>
2016-02-07 23:48:13 -0500

    Create page for User-subscribed Projects

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,4 +1,6 @@
 class ProjectController < ApplicationController
+  before_action :authenticate_user!, only: [:list]
+
   def index
     @projects = Project.all
   end
@@ -12,5 +14,11 @@ class ProjectController < ApplicationController
       @joined = current_user.projects.include?(@project)
     end
     @users = @project.users.order('created_at desc').first(10)
+  end
+
+  def list
+    if !current_user.nil?
+      @projects = current_user.projects
+    end
   end
 end
