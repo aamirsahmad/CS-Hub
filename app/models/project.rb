8840e43079f728024c316e14a80046346842a4b3
Philip Alekseev <phil.alekseev@gmail.com>
2016-02-08 15:43:10 -0500

    Create Review functionality for Projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,10 +4,15 @@ class Project < ActiveRecord::Base
   has_many :tasks
   has_many :subscriptions
   has_many :users, through: :subscriptions
+  has_many :reviews
   has_attached_file :image, styles: { medium: "680x300>", thumb: "170x75>" }, default_url: "/images/:style/missing.png"
   
   validates :name, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 500 }
   validates :price, presence: true, numericality: {only_integer: true}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  def average_rating
+    reviews.blank? ? 0 : reviews.average(:star).round(2)
+  end
 end
