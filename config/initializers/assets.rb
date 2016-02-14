0c7e03d09791b1647fe94eb149fb45fc8a4cefd5
Philip Alekseev <phil.alekseev@gmail.com>
2016-02-14 01:32:07 -0500

    Fix #2 Assets Precompilation for Production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.01'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w('*.js', forem.css forem.js normalize.css homepage-canvas.css )
+Rails.application.config.assets.precompile += %w( forem.css forem.js normalize.css homepage-canvas.css EasePack.min.js homepage-canvas.js rAF.js TweenLite.min.js )
