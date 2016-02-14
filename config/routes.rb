33abcb0f084e2fefb07cc54fce0f1b5955e47ddc
Philip Alekseev <phil.alekseev@gmail.com>
2016-02-14 00:37:53 -0500

    Fix Mobile View and minor CSS fixes

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,9 @@ Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'},
               :controllers => {:omniauth_callbacks => "omniauth_callbacks"}
   get 'pages/about'
-  root 'pages#about'
+  root 'pages#home'
   get 'pages/userlist'
+  get 'pages/home'
   get 'myprojects' => 'project#list'
   post '/free' => 'charge#free'
   
