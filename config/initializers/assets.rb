a69c9874f95904d2937f10e3d512a903ebc103ee
Philip Alekseev <phil.alekseev@gmail.com>
2016-02-20 23:32:43 -0500

    Layout & CSS changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.03'
+Rails.application.config.assets.version = '1.04'
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.03'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( forem.css forem.js normalize.css homepage-canvas.css EasePack.min.js jquery.raty/* homepage-canvas.js rAF.js TweenLite.min.js)
+Rails.application.config.assets.precompile += %w( forem.css forem.js project.scss task.scss uploads.scss normalize.css homepage-canvas.css EasePack.min.js jquery.raty/* homepage-canvas.js rAF.js TweenLite.min.js)
